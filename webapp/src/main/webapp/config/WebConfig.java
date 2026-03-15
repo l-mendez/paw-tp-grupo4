7b905b05072c06a67f9d6227ef77534bac2533fd
Lorenzo Méndez <lmendez@itba.edu.ar>
2026-03-15 16:45:50 -0300

    continued with the setup

---
+++ webapp/src/main/webapp/config/WebConfig.java
@@ -0,0 +1,8 @@
+package ar.edu.itba.paw.config
+import org.springframework.context. annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+  public class WebConfig {
+  }
