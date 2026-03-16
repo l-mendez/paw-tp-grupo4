4c2997bc316bd6f862aa46119b53996935efece1
Lorenzo Méndez <lmendez@itba.edu.ar>
2026-03-16 12:36:57 -0300

    Add service-contracts module and update project structure
    - Create service-contracts module with UserService interface
    - Refactor services to implement interface via UserServiceImpl
    - Update Java version from 17 to 21
    - Add spring-context dependency
    - Wire services with @Autowired in HomeController
    - Update WebConfig to scan services package
    - Improve .gitignore with IDE, OS, and Maven files

---
+++ services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.paw.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public Object createUser(final String email) {
+        return email;
+    }
+}
--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +9,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView home() {
